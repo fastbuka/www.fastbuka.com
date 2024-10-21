11cd5c0ad324001c4297e58305f30817858ebbff
chidube anike <anikechidube@gmail.com>
2024-10-21 03:34:39 +0100

    Created utility function to handle fallbacks for avatar and first_name

---
+++ src/utils/defaults.ts
@@ -0,0 +1,10 @@
+// sets a default avatar if none is provided
+export function getDefaultAvatar(avatar: string): string {
+    //using optional chaining to avoid throwing an error if avatar is null or undefined
+  return avatar?.trim() ? avatar : "/public/images/profile.png";
+}
+
+// sets a fallback name if first_name is not provided or an empty string
+export function getDefaultFirstName(first_name: string): string {
+    return first_name?.trim() ? first_name : "Guest user";
+}
