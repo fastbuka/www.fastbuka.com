1a4efd1c0b2c29cbfabd7007eb13716637630d0d
chidube anike <anikechidube@gmail.com>
2024-10-21 12:34:26 +0100

    fixed image path

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,7 +1,7 @@
 // sets a default avatar if none is provided
 export function getDefaultAvatar(avatar: string): string {
     //using optional chaining to avoid throwing an error if avatar is null or undefined
-  return avatar?.trim() ? avatar : "/public/images/profile.png";
+  return avatar?.trim() ? avatar : "/images/profile.png";
 }
 
 // sets a fallback name if first_name is not provided or an empty string
