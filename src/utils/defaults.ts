5f8d9a1b00d86a432db8ff1e8be0145b395462bf
chidube anike <anikechidube@gmail.com>
2024-10-25 01:52:02 +0100

    preventing typescript errors

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,10 +1,10 @@
 // sets a default avatar if none is provided
-export function getDefaultAvatar(avatar: string): string {
+export function getDefaultAvatar(avatar: string | undefined): string {
     //using optional chaining to avoid throwing an error if avatar is null or undefined
   return avatar?.trim() ? avatar : "/images/profile.png";
 }
 
 // sets a fallback name if first_name is not provided or an empty string
-export function getDefaultFirstName(first_name: string): string {
+export function getDefaultFirstName(first_name: string | undefined): string {
     return first_name?.trim() ? first_name : "Guest user";
 }
