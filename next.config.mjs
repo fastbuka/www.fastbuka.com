b16a542273f5701a4a2c8d92132744153b4ab4e4
chidube anike <anikechidube@gmail.com>
2024-10-25 03:52:29 +0100

    configurations for various  image urls domains and subdomains

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,26 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
