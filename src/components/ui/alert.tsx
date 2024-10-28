be87c86e36cd6ef0a63d9115f6d5aa79e3c2b923
chidube anike <anikechidube@gmail.com>
2024-10-28 14:04:57 +0100

    modified component to support different variants thus reusability

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,36 +1,53 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+// import { useEffect } from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const alertVariants = cva(
   "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
   {
     variants: {
       variant: {
-        default: "bg-background text-foreground",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+        success: "bg-green-100 text-green-600 text-xl",
+        info: "bg-blue-100 text-blue-600 text-xl",
+        warning: "bg-yellow-100 text-yellow-600 text-xl",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "info",
     },
   }
-)
+);
 
 const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
->(({ className, variant, ...props }, ref) => (
-  <div
-    ref={ref}
-    role="alert"
-    className={cn(alertVariants({ variant }), className)}
-    {...props}
-  />
-))
-Alert.displayName = "Alert"
+>(({ className, variant, ...props }, ref) => {
+  // const [show, setShow] = React.useState(true);
+  // function to set a timeout to remove the alert
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setShow(false);
+  //   }, 7000);
+
+  //   return () => clearTimeout(timer);
+  // }, [show]);
+
+  return (
+    
+      <div
+        ref={ref}
+        role="alert"
+        className={cn(alertVariants({ variant }), className)}
+        {...props}
+      />
+  );
+});
+Alert.displayName = "Alert";
 
 const AlertTitle = React.forwardRef<
   HTMLParagraphElement,
@@ -41,8 +58,8 @@ const AlertTitle = React.forwardRef<
     className={cn("mb-1 font-medium leading-none tracking-tight", className)}
     {...props}
   />
-))
-AlertTitle.displayName = "AlertTitle"
+));
+AlertTitle.displayName = "AlertTitle";
 
 const AlertDescription = React.forwardRef<
   HTMLParagraphElement,
@@ -53,7 +70,7 @@ const AlertDescription = React.forwardRef<
     className={cn("text-sm [&_p]:leading-relaxed", className)}
     {...props}
   />
-))
-AlertDescription.displayName = "AlertDescription"
+));
+AlertDescription.displayName = "AlertDescription";
 
-export { Alert, AlertTitle, AlertDescription }
+export { Alert, AlertTitle, AlertDescription };
