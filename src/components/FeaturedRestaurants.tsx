abd3f16f93de9f2effdd34ba1533cd46193a1681
chidube anike <anikechidube@gmail.com>
2024-10-29 12:07:30 +0100

    using skeleton loader and alert component to handle api errors

--- src/components/FeaturedRestaurants.tsx
+++ src/components/FeaturedRestaurants.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { useGetRestaurants } from "@/queries/frontPage";
+import { Skeleton } from "@radix-ui/themes";
+import { TriangleAlert } from "lucide-react";
+import { Alert, AlertDescription } from "./ui/alert";
 
 interface Restaurant {
   id: number;
@@ -22,15 +25,23 @@ interface Restaurant {
 export default function FeaturedRestaurants({
   title = "Featured Restaurants",
 }) {
-  const { data: restaurants, isLoading, error } = useGetRestaurants();
-
-  if (isLoading) {
-    return <h2>Loading restaurants...</h2>;
-  }
+  const {
+    data: restaurants,
+    isLoading,
+    isFetching,
+    error,
+  } = useGetRestaurants();
 
   if (error) {
     console.error(error);
-    return <h2>Failed to load restaurants. Please try again.</h2>;
+    return (
+      <Alert variant={"destructive"} className="mx-auto">
+        <TriangleAlert color="red" size={28} className="mx-4" />
+        <AlertDescription>
+          Failed to fetch restaurants. Please try again.
+        </AlertDescription>
+      </Alert>
+    );
   }
 
   return (
@@ -39,26 +50,41 @@ export default function FeaturedRestaurants({
       {/* Light blue background */}
       <div className="max-w-7xl mx-auto text-center">
         <h2 className="text-3xl sm:text-4xl font-bold text-green-600 mb-2 sm:mb-4">
-          {title}
+          <Skeleton loading={isLoading}>{title}</Skeleton>
         </h2>
         <p className="text-gray-600 max-w-2xl mx-auto mb-6 sm:mb-10 text-sm sm:text-base">
-          Our diverse selection of restaurants, ranging from local favorites to
-          renowned hotspots, guarantees a delicious variety to satisfy every
-          taste and preference.
+          <Skeleton loading={isLoading}>
+            Our diverse selection of restaurants, ranging from local favorites
+            to renowned hotspots, guarantees a delicious variety to satisfy
+            every taste and preference.
+          </Skeleton>
         </p>
 
         {/* Restaurant grid */}
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 sm:gap-4 md:gap-6">
-          {restaurants.map((restaurant: Restaurant) => (
-            <div
-              key={restaurant.id}
-              className="bg-[#B0E8D4] rounded-lg p-3 sm:p-4 md:p-6 text-center shadow-md hover:shadow-lg transition-shadow duration-300 flex items-center justify-center"
-            >
-              <h3 className="text-sm sm:text-base md:text-lg font-semibold text-gray-800">
-                {restaurant.name}
-              </h3>
-            </div>
-          ))}
+          {isLoading || isFetching
+            ? Array.from({ length: 10 }).map((_, index) => (
+                <Skeleton loading={isLoading || isFetching}>
+                  <div
+                    key={index}
+                    className="bg-[#B0E8D4] rounded-lg p-3 sm:p-4 md:p-6 text-center shadow-md flex items-center justify-center"
+                  >
+                    <div className="h-5 w-24 bg-gray-200"></div>
+                  </div>
+                </Skeleton>
+              ))
+            : restaurants.map((restaurant: Restaurant) => (
+                <div
+                  key={restaurant.id}
+                  className="bg-[#B0E8D4] rounded-lg p-3 sm:p-4 md:p-6 text-center shadow-md hover:shadow-lg transition-shadow duration-300 flex items-center justify-center"
+                >
+                  <h3 className="text-sm sm:text-base md:text-lg font-semibold text-gray-800">
+                    <Skeleton loading={isLoading || isFetching}>
+                      {restaurant.name}
+                    </Skeleton>
+                  </h3>
+                </div>
+              ))}
         </div>
       </div>
     </section>
