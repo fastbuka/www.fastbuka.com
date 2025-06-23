8d083bdef311500dc1b058ec9fb63d3cbfe21f9a
unknown <pebuka351@gmail.com>
2025-06-23 19:04:55 +0100

    Made Adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.className} h-screen overflow-y-auto @container antialiased`}
-      >
+      <body className={`${poppins.className} @container antialiased`}>
         <Providers>
           {children} <Authentication />
           <AuthenticatedModals />
