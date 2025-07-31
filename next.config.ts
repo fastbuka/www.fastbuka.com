8b578c6d955d8330a3fb78dcc427fa6414fff8f6
tesimune <tesimune@gmail.com>
2025-07-31 05:53:46 +0100

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,24 @@
 import type { NextConfig } from "next";
 
-const bucketURL = process.env.NEXT_PUBLIC_BUCKET_URL;
-const bucketEnv = process.env.NEXT_PUBLIC_STORAGE_ENV;
+const bucketURL = process.env.NEXT_PUBLIC_BUCKET_URL || "";
+const bucketEnv = process.env.NEXT_PUBLIC_STORAGE_ENV || "";
+
+const parsedURL = new URL(bucketURL);
+const hostname = parsedURL.hostname;
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
-      new URL(
-        `${bucketURL}/${bucketEnv}/user/a33c7be8-236a-462d-bbf7-bdd89131f7f5/images/**`
-      ),
+      {
+        protocol: "http",
+        hostname: hostname,
+        pathname: `/${bucketEnv}/**`,
+      },
+      {
+        protocol: "https",
+        hostname: hostname,
+        pathname: `/${bucketEnv}/**`,
+      },
     ],
   },
 };
