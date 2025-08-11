a6bb127a5d81287192abde1e5dbef4d36acdf968
tesimune <tesimune@gmail.com>
2025-08-11 11:07:47 +0100

    Update

--- src/components/modals/Success.tsx
+++ src/components/modals/Success.tsx
@@ -17,7 +17,7 @@ export default function Success() {
     <div className="w-[472px] @max-2xl:w-full max-w-full flex flex-col items-center">
       <h3 className="text-center font-medium text-[#111111] text-base 2xl:text-xl mb-9 2xl:mb-[50px]">
         {ongoingTransfer && "Funding Options"}
-        {activeOrder && "Order placed"}
+        {activeOrder?.payment_status == "SUCCESSFUL" && "Order placed"}
       </h3>
       <Image src="/images/success.svg" alt="" width={140} height={140} />
       {(ongoingTransfer || ongoingTopup) && (
@@ -29,7 +29,7 @@ export default function Success() {
         </p>
       )}
 
-      {activeOrder && (
+      {activeOrder?.payment_status == "SUCCESSFUL" && (
         <p className="mt-4 mb-9 2xl:mb-[50px] 2xl:mt-6 text-center max-w-[294px] font-normal text-sm 2xl:text-base text-[#5D5D5D]">
           You have successfully placed an order, Thank you for using FastBuka!
         </p>
